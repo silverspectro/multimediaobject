59a7d82e9477e5d315ec1e511621abbad323a70b
B-Core <bcore31@gmail.com>
2017-01-27 00:55:32 +0100

    Add MO.init test

---
+++ test/MultimediaObject/MO.init.test.js
@@ -0,0 +1,26 @@
+/* globals MultimediaObject */
+
+describe("MultimediaObject init", () => {
+  const types = [
+    "div",
+    "input",
+    "form",
+    "img",
+    "iframe",
+    "video",
+    "audio",
+  ];
+
+  types.forEach((type) => {
+    const ob = new MultimediaObject(type);
+    it(`should generate a DOMElement based on the type ${type}`, () => {
+      expect(ob.element.nodeName).toEqual(type.toUpperCase());
+    });
+  });
+
+  const ob2 = new MultimediaObject();
+
+  it("should append the element to the body", () => {
+    expect(document.getElementById(ob2.name)).toEqual(ob2.element);
+  });
+});
