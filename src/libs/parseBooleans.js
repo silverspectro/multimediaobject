9724dfc8490f6b248b18365ca85d557eef83dcb4
B-Core <bcore31@gmail.com>
2018-05-25 12:07:53 +0200

    Externalized libs and modules

--- src/libs/parseBooleans.js
+++
@@ -1,9 +0,0 @@
-export default string => {
-  if (string === '') {
-    return true;
-  }
-  if (typeof string === 'undefined' || string === 'false' || Boolean(string) === false) {
-    return false;
-  }
-  return Boolean(string);
-};
